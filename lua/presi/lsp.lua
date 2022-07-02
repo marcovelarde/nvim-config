811b1bbc6a9bbc6726d90c9c8262be4b015d71d3
Marco Velarde <marco.avelarde7@gmail.com>
2022-07-02 15:28:05 -0500

    refactor: update cmp config for : cmd

--- lua/presi/lsp.lua
+++ lua/presi/lsp.lua
@@ -53,11 +53,15 @@ cmp.setup.cmdline('/', {
 -- ':' cmd setup
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path'}
-  }, {
-    { name = 'buffer' }
-  })
+  sources = cmp.config.sources(
+    {
+      { name = 'path'}
+    }
+    --,
+    -- {
+    --   { name = 'buffer' }
+    -- }
+  )
 })
 
 local capabilities = cmp_nvim.update_capabilities(
