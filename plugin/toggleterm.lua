27882fa1d70b7c9b1e30345520572cb9140f0cfb
Marco Velarde <marco.avelarde7@gmail.com>
2022-12-15 21:40:15 -0500

    refactor: update toggleterm config

--- plugin/toggleterm.lua
+++ plugin/toggleterm.lua
@@ -7,6 +7,7 @@ require('toggleterm').setup {
     -- Send <Esc> when toggling term
     -- vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'm', true)
   end,
+  direction = 'float',
   start_in_insert = false,
   -- open_mapping = [[<c-\>]],  -- <C-T> seted in maps
   open_mapping = [[<c-t>]],
