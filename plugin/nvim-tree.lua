4da562b1853cc89b505f5092a0cf8edce9b55575
Marco Velarde <marco.avelarde7@gmail.com>
2023-04-13 17:27:58 -0500

    refactor: remove deprecated open_on_setup nvim-tree option

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,7 +1,6 @@
 require'nvim-tree'.setup {
   disable_netrw       = true,
   hijack_netrw        = true,
-  open_on_setup       = false,
   open_on_tab         = false,
   hijack_cursor       = true,
   sync_root_with_cwd  = false,
